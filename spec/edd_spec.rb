10aa87660fdbf65e180d5a52962f852a0a8f0b0c
eddKerr <eddkerr@pathosethos.com>
2016-01-29 16:36:39 -0500

    a few more working tests

--- spec/edd_spec.rb
+++ spec/edd_spec.rb
@@ -2,20 +2,59 @@ require_relative '../src/machine'
 require_relative '../src/snack'
 
 describe "Machine" do
+  emptyStock = []
+
+  ### some snacks
+  chips = Snack.new('Chips', 1.00)
+  snickers = Snack.new('Snickers', 0.75)
+  caviar = Snack.new('Caviar', 500)
+  gum = Snack.new('Gum', 0.37)
+
+  ## some currency
+  quarter = Currency.new('Quarter', 0.25)
+  dime = Currency.new('Dime', 0.10)
+  nickel = Currency.new('Nickel', 0.05)
+  penny = Currency.new('Penny', 0.01)
+
+  americanCoins = [quarter, dime, nickel, penny]
 
   it "must vend a snack" do
-    machine = Machine.new()
-    snack = Snack.new('Chips', 1)
-    expect(machine.vend(snack)).to eq(snack)
+    machine = Machine.new([chips])
+    expect(machine.vend(chips)).to eq(chips)
   end
 
-  it "must be able to total currencies in a given payment" do
-    quarter = Currency.new('Quarter', 0.25)
-    machine = Machine.new()
+  it "must be able to total the value of the currency in a given payment" do
+    machine = Machine.new(emptyStock)
     payment = [quarter, quarter]
     expect(machine.countChange(payment)).to eq(0.50)
   end
-  
+
+  it "must be able to find the highest note of a given currency set that is smaller than the payment due" do
+    machine = Machine.new(emptyStock)
+    newCurrencySet = [quarter, dime, nickel, penny]
+    expect(machine.findHighestNote(newCurrencySet, 0.26)).to eq(quarter)
+    expect(machine.findHighestNote(newCurrencySet, 0.25)).to eq(quarter)
+    expect(machine.findHighestNote(newCurrencySet, 0.24)).to eq(dime)
+
+  end
+
+
+
+  it "count and inventory the amount of each item it has" do
+    itemsInStock = [chips, chips, chips, snickers]
+    machine = Machine.new(itemsInStock)
+    expect(machine.checkInventory(chips)).to be true
+    expect(machine.checkInventory(caviar)).to be false
+  end
+
+  it "must remove an item from its inventory after vending it" do
+    itemsInStock = [chips, chips, gum, snickers]
+    machine = Machine.new(itemsInStock)
+    expect(machine.itemsInStock.length).to eq(4)
+    machine.vend(gum)
+    expect(machine.itemsInStock.length).to eq(3)
+  end
+
 end
 
 describe "Snack" do
@@ -33,4 +72,5 @@ describe "Currency" do
     expect(quarter.value).to eq(0.25)
   end
 end
+
 
--- src/machine.rb
+++ src/machine.rb
@@ -1,12 +1,18 @@
-
-
 class Machine
-  def initialize
-    return
+  attr_reader :itemsInStock
+
+  def initialize(itemsInStock)
+    @itemsInStock = itemsInStock
   end
 
   def vend(itemRequested)
-    item = itemRequested
+    for item in @itemsInStock
+      if item == itemRequested
+        @itemsInStock.delete(item)
+        return itemRequested
+        return
+      end
+    end
     return item
   end
 
@@ -16,6 +22,25 @@ class Machine
       changeDue += currency.value
     end
     return changeDue
+  end
+
+  def checkInventory(item)
+    for stockedItem in @itemsInStock do
+      if stockedItem == item
+        return true
+      end
+      return false
+    end
+  end
+
+  def findHighestNote(currencySet, changeDue)
+    currencyChoices = currencySet.sort! { |a,b| b.value <=> a.value}
+    for currency in currencyChoices
+      if currency.value > changeDue
+        currencyChoices.delete(currency)
+      end
+    end
+    return currencyChoices[0]
   end
 
 end
